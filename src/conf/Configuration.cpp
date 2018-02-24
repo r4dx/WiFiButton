5621e736b06206dd39549b7aaea2cc72b7e98a51
r4dx <r4dx@bk.ru>
2018-02-25 01:07:00 +0300

    configuration.h -> Configuration.h, and now it's a real class

---
+++ src/conf/Configuration.cpp
@@ -0,0 +1,44 @@
+#include "Configuration.h"
+
+namespace wifi_button {
+	namespace configuration {
+		SaveResponse Configuration::save() {
+			if (ssid.size() > 32)
+				return SaveResponse::SSID_TOO_LONG;
+			if (password.size() > 128)
+				return SaveResponse::PASSWORD_TOO_LONG;
+			if (url.size() > 2083)
+				return SaveResponse::URL_TOO_LONG;
+			if (headers.size() + body.size() + url.size() + password.size() + ssid.size() + 5 >= 8000)
+				return SaveResponse::OVERALL_SIZE_TOO_BIG;
+
+			storage.write_uint32(magic);
+			storage.write_string(ssid);
+			storage.write_string(password);
+			storage.write_string(url);
+			storage.write_uint8((uint8_t)method);
+			storage.write_string(headers);
+			storage.write_string(body);
+			return SaveResponse::OK;
+
+		}
+
+		std::shared_ptr<Configuration> Configuration::load(sentinel::storage::IStorage& storage) {
+			if (storage.read_uint32() != magic)
+				return nullptr;
+
+			std::shared_ptr<Configuration> result = std::make_shared<Configuration>(storage);
+
+			result->ssid = storage.read_string();
+			result->password = storage.read_string();
+			result->url = storage.read_string();
+			result->method = (sentinel::web::Method)storage.read_uint8();
+			result->headers = storage.read_string();
+			result->body = storage.read_string();
+
+			return result;
+		}
+
+		Configuration::Configuration(sentinel::storage::IStorage& storage) : storage(storage) { }
+	}
+}
---
+++ src/conf/Configuration.h
@@ -0,0 +1,36 @@
+#pragma once
+#include <string>
+#include "common/sentinel/web/Method.h"
+#include "common/sentinel/storage/IStorage.h"
+#include <memory>
+#include <stdint.h>
+
+namespace wifi_button {
+    namespace configuration {
+		enum SaveResponse {
+			OK,
+			SSID_TOO_LONG,
+			URL_TOO_LONG,
+			PASSWORD_TOO_LONG,
+			OVERALL_SIZE_TOO_BIG
+		};
+
+        class Configuration {
+		public:
+			std::string ssid;
+			std::string password;
+			std::string url;
+			sentinel::web::Method method;
+			std::string headers;
+			std::string body;
+
+			SaveResponse save();
+			static std::shared_ptr<Configuration> load(sentinel::storage::IStorage& storage);
+			Configuration(sentinel::storage::IStorage& storage);
+		private:
+			sentinel::storage::IStorage& storage;
+
+			static const uint32_t magic = 0x0ddfee1;
+        };
+	}
+}
